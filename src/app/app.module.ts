361f82cf15642a3ff0a679815810f1b2034a95a2
katerina <sk.katherine@gmail.com>
2018-09-04 08:17:27 -0500

    added angularfire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
+
+const config = {
+  apiKey: "AIzaSyCq_8sc1_OeUBViQkmazhoyL97HqMtmrDI",
+  authDomain: "chat-psybercity.firebaseapp.com",
+  databaseURL: "https://chat-psybercity.firebaseio.com",
+  projectId: "chat-psybercity",
+  storageBucket: "chat-psybercity.appspot.com",
+  messagingSenderId: "419036519980"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
